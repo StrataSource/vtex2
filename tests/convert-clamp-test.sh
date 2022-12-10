debcea2cde46a916c746e32d996ec50edac31eb8
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2022-12-10 01:45:38 -0800

    cli: -? instead of -h for short option to show help text

--- tests/convert-clamp-test.sh
+++ tests/convert-clamp-test.sh
@@ -3,5 +3,5 @@ set -e
 cd "$(dirname "$0")"
 
 echo "Convert JPEG -> RGB888 512x256"
-../build/vtex2 convert -f rgb888 --width 512 --height 256 -o convert-clamp-test.vtf funny-cat-2.jpg
+../build/vtex2 convert -f rgb888 -w 512 -h 256 -o convert-clamp-test.vtf funny-cat-2.jpg
 ../build/vtfview convert-clamp-test.vtf
